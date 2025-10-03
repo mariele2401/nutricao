2a2ad974895fa2b1b12033f41030210fa1f3ea57
Mariele <mariele.florencio@escola.pr.gov.br>
2025-10-03 10:57:51 -0300

    3

--- js/principal.js
+++ js/principal.js
@@ -7,4 +7,7 @@ console.log(titulo.textContent);
 titulo.textContent = "Aparecida Nutricionista";
 
 var paciente = document.querySelector("#primeiro-paciente");
-console.log(paciente)
+var tdPeso=paciente.querySelector("info-peso");
+
+console.log(paciente);
+console.log(tdPeso);
