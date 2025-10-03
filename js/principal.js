3659841732485eed989442ab043ff525729ad11a
Mariele <mariele.florencio@escola.pr.gov.br>
2025-10-03 11:07:18 -0300

    10

--- js/principal.js
+++ js/principal.js
@@ -10,4 +10,4 @@ var paciente = document.querySelector("#primeiro-paciente");
 var tdPeso=paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 console.log(paciente);
-console.log(tdPeso);
+console.log(Peso);
