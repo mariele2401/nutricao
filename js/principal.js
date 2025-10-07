21387c18ada629bc242e26a9b8a2161434f10856
Mariele <mariele.florencio@escola.pr.gov.br>
2025-10-07 09:31:17 -0300

    11

--- js/principal.js
+++ js/principal.js
@@ -1,16 +1,19 @@
 var titulo = document.querySelector("h1");
-
 console.log(titulo);
-
 console.log(titulo.textContent);
-
 titulo.textContent = "Aparecida Nutricionista";
 
 var paciente = document.querySelector("#primeiro-paciente");
 console.log(paciente);
+
+var tdnome = paciente.querySelector(".info-nome");
+var nome = tdNome.textContent;
+console.log(nome);
+
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 console.log(peso);
+
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 console.log(altura);
