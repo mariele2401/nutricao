56b662c0184486bec8f29d5c552f4824ab8a7d9c
Mariele <mariele.florencio@escola.pr.gov.br>
2025-10-07 09:42:55 -0300

    13

--- js/principal.js
+++ js/principal.js
@@ -17,3 +17,8 @@ console.log(peso);
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 console.log(altura);
+
+var imc = peso / (altura*altura);
+console.log(imc);
+varImc = paciete.querySelector(".info-imc");
+tdImc.textContent + imc.toFixed(2)
